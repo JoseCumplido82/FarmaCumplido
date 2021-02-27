e50464ebbfd6f5cf1171e199a1bc7165d6d866aa
Jose <josecumplido82@gmail.com>
2021-02-28 00:14:58 +0100

    datos cambiados en la configuracion para que funcione en android studio y no en phpmyadmin instituto

--- app/src/main/java/com/example/farmacumplido/modelos/ConfiguracionDB.java
+++ app/src/main/java/com/example/farmacumplido/modelos/ConfiguracionDB.java
@@ -2,14 +2,16 @@ package com.example.farmacumplido.modelos;
 
 public class ConfiguracionDB {
     public static final String HOSTDB = "10.0.2.2";
-    public static final String NOMBREDB = "medicamentosDB";
-    public static final String USUARIODB = "root";
-    public static final String CLAVEDB = "1234";
-    private static final String OPCIONES = "?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
-    public static final String PUERTOMYSQL = "3306";
-    public static final String URLMYSQL = "jdbc:mysql://"+ HOSTDB + ":" + PUERTOMYSQL+"/" + NOMBREDB + OPCIONES;
     //public static final String HOSTDB = "infsalinas.sytes.net";
-    //public static final String USUARIODB = "root";
+    public static final String NOMBREDB = "medicamentosdb";
+    public static final String USUARIODB = "root";
+    //public static final String USUARIODB = "damserver1";
+    public static final String CLAVEDB = "1234";
     //public static final String CLAVEDB = "dam1234";
-    // public static final String PUERTOMYSQL = "5306"
+   private static final String OPCIONES = "?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
+    //private static final String OPCIONES = "?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC&allowPublicKeyRetrieval=true&useSSL=false";
+    public static final String PUERTOMYSQL = "3306";
+    //public static final String PUERTOMYSQL = "5306";
+    public static final String URLMYSQL = "jdbc:mysql://"+ HOSTDB + ":" + PUERTOMYSQL+"/" + NOMBREDB + OPCIONES;
+
 }
